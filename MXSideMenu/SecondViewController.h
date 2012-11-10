5ecd5cb38946978045cb99121606fb224324c5de
Miguel Fermin <miguelx@gmail.com>
2012-11-10 19:37:15 +0100

    Revert "Revert "Initial commit""
    This reverts commit 2beee9d763173d3a109aec5ecf9edba51006eb1c.

---
+++ MXSideMenu/SecondViewController.h
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.h
+//  MXSideMenu
+//
+//  Created by Miguel Fermin on 09/11/12.
+//  Copyright (c) 2012 Miguel Fermin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+- (IBAction)toggleMenu:(id)sender;
+
+@end
